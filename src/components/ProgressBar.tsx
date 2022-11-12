37dbd22082df17103c3f7c7c53b7e212908ab2d9
fior <marcin.lukasz.fiorek@gmail.com>
2022-11-12 14:09:22 +0100

    Add egg image to ProgresBar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface ProgressBarProps {
   size?: number;
   progress?: number;
@@ -20,19 +22,21 @@ const ProgressBar: React.FC<ProgressBarProps> = (props) => {
   const center = size / 2;
   const radius =
     center - (trackWidth > indicatorWidth ? trackWidth : indicatorWidth);
-  const dashArray = 2 * Math.PI * radius;
-  const dashOffset = dashArray * ((100 - progress) / 100);
+  const dashArray = (2 * Math.PI * radius * 2) / 3;
+  const dashOffset = (dashArray * ((100 - progress) / 100) * 2) / 3;
 
   return (
     <>
       <div className="relative" style={{ width: size, height: size }}>
-        <svg className="rotate-90" style={{ width: size, height: size }}>
+        <svg className="rotate-[150deg]" style={{ width: size, height: size }}>
           <circle
             className="fill-transparent stroke-neutral-600"
             cx={center}
             cy={center}
             r={radius}
             strokeWidth={trackWidth}
+            strokeDasharray={dashArray}
+            strokeLinecap={indicatorCap}
           />
           <circle
             className="fill-transparent stroke-lime-600"
@@ -46,7 +50,17 @@ const ProgressBar: React.FC<ProgressBarProps> = (props) => {
           />
         </svg>
 
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Image
+            src="/Egg.webp"
+            alt="Pokemon egg image"
+            width={"100"}
+            height={"100"}
+            style={{ imageRendering: "pixelated" }}
+            priority={true}
+          />
+        </div>
+        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
           <div className="flex text-4xl font-extrabold">
             <span>{label}</span>
           </div>
